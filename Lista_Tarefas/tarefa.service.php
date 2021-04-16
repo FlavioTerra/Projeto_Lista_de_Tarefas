012dc1103169edcb5ed02a4a8af6c87d2dad002f
Flavio Moreira Terra <FlavioTerraMoreira@gmail.com>
2021-04-16 19:24:06 -0300

    Adicionado a classe TarefaService

---
+++ Lista_Tarefas/tarefa.service.php
@@ -0,0 +1,23 @@
+<?php
+
+    // CRUD
+    class TarefaService {
+        
+        public function inserir() { // CREATE
+
+        }
+
+        public function recuperar() { // READ
+
+        }
+
+        public function atualizar() { // UPDATE
+
+        }
+
+        public function remover() { // DELETE
+
+        }
+    }
+
+?>
